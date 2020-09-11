a98079e667e26481b9537e81e3320fa0bafce1bf
Nasar-uddin <nasaruddin171@gmail.com>
2020-09-11 12:10:33 +0600

    marker added in the map. Now add a on click event

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  GoogleMapController mapController;
+  static final LatLng _center = LatLng(23.705617, 90.545431);
+  void _onMapCreated(GoogleMapController controller){
+    mapController = controller;
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Text('I am a map app'),
+    print(_center.latitude);
+    return new Scaffold(
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(target: _center,zoom: 13.15),
+        mapType: MapType.terrain,
+        markers: {myPos},
       ),
     );
   }
+  Marker myPos = Marker(
+    markerId: MarkerId('Md Nasar uddin'),
+    position: _center,
+    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+    infoWindow: InfoWindow(title: 'Nasar uddin'),
+    onTap: (){
+      print('Taped');
+    }
+  );
 }
